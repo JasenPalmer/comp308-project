21bcede5fd0cb630b484d31ee01d182ef283ecdb
Matt Byers <matt_byers@live.com>
2017-06-05 16:00:12 +1200

    Added new perlin noise class and header, modified cmakelists to build these also.

---
+++ work/src/perlin_noise.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "cgra_math.hpp"
+#include "opengl.hpp"
+
+class PerlinNoise {
+    
+private:
+    //Not sure what methods i'll need for this yet...
+public:
+    
+    
+};
